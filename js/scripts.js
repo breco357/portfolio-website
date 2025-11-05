49cdc2e75366eb222ce9150e7ea91898781d80e6
breco357 <breco357@yahoo.com>
2025-11-05 06:10:21 -0700

    Responsive Menu with JS
    • Lucide icon library linked throughout.
    • JQuery library added to each page
    • JS function included for a more responsive menu
    • CSS updated for menu functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
 console.log("HI! Welcome to my portfolio site. Reach out if you'd like to work together!");
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
